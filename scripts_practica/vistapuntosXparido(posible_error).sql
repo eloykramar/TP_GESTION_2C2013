c5a65209de7b4f93ce997ca2ad30fa94cf57e885
alee91 <alee_91@live.com>
2013-11-11 00:28:31 -0200

    corrijo vista

--- scripts_practica/vistapuntosXparido(posible_error).sql
+++ scripts_practica/vistapuntosXparido(posible_error).sql
@@ -1,41 +1,90 @@
-
-
-
 CREATE VIEW PUNTOS_PARTIDO AS
-(SELECT PART_TORNEO, PART_CODIGO, PART_LOCAL EQUIPO,
- CASE WHEN ((SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_LOCAL) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_VISITANTE))
->(SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_VISITANTE) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_LOCAL)))
-THEN 3
- WHEN ((SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_LOCAL) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_VISITANTE))
-<(SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_VISITANTE) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_LOCAL)))
-THEN 0
- WHEN ((SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_LOCAL) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_VISITANTE))
-=(SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_VISITANTE) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_LOCAL)))
-THEN 1
-END AS PUNTOS
-FROM PARTIDO
+SELECT     PART_TORNEO as TORNEO, PART_CODIGO as PARTIDO, PART_LOCAL EQUIPO, CASE WHEN
+                          ((SELECT     COUNT(*)
+                              FROM         (PARTIDO INNER JOIN
+                                                    DETALLE_PARTIDO ON
+                                                    (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO))
+                              WHERE     (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_LOCAL) OR
+                                                    (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_VISITANTE)) >
+                          (SELECT     COUNT(*)
+                            FROM          (PARTIDO INNER JOIN
+                                                   DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO))
+                                                   
+                            WHERE      (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_VISITANTE) OR
+                                                   (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_LOCAL))) THEN 3 WHEN
+                          ((SELECT     COUNT(*)
+                              FROM         (PARTIDO INNER JOIN
+                                                    DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO))
+                                                   
+                              WHERE     (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_LOCAL) OR
+                                                    (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_VISITANTE)) <
+                          (SELECT     COUNT(*)
+                            FROM          (PARTIDO INNER JOIN
+                                                   DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO))
+                                                   
+                            WHERE      (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_VISITANTE) OR
+                                                   (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_LOCAL))) THEN 0 WHEN
+                          ((SELECT     COUNT(*)
+                              FROM         (PARTIDO INNER JOIN
+                                                    DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO))
+                                                   
+                              WHERE     (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_LOCAL) OR
+                                                    (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_VISITANTE)) =
+                          (SELECT     COUNT(*)
+                            FROM          (PARTIDO INNER JOIN
+                                                   DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO))
+                                                   
+                            WHERE      (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_VISITANTE) OR
+                                                   (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_LOCAL))) THEN 1 END AS PUNTOS
+FROM         PARTIDO p
 UNION
-SELECT PART_TORNEO, PART_CODIGO, PART_VISITANTE EQUIPO,
- CASE WHEN ((SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_LOCAL) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_VISITANTE))
-<(SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_VISITANTE) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_LOCAL)))
-THEN 3
- WHEN ((SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_LOCAL) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_VISITANTE))
->(SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_VISITANTE) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_LOCAL)))
-THEN 0
- WHEN ((SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_LOCAL) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_VISITANTE))
-=(SELECT COUNT(*) FROM (PARTIDO INNER JOIN DETALLE_PARTIDO ON DEPA_PARTIDO=PART_CODIGO)  WHERE (DEPA_ACCION=1 AND DEPA_EQUIPO=PART_VISITANTE) OR
-(DEPA_ACCION=10 AND DEPA_EQUIPO=PART_LOCAL)))
-THEN 1
-END AS PUNTOS
-FROM PARTIDO)
+SELECT     PART_TORNEO, PART_CODIGO, PART_VISITANTE EQUIPO, CASE WHEN
+                          ((SELECT     COUNT(*)
+                              FROM         (PARTIDO INNER JOIN
+                                                    DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO)
+                                                    )
+                              WHERE     (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_LOCAL) OR
+                                                    (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_VISITANTE)) <
+                          (SELECT     COUNT(*)
+                            FROM          (PARTIDO INNER JOIN
+                                                   DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO)
+                                                    )
+                            WHERE      (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_VISITANTE) OR
+                                                   (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_LOCAL))) THEN 3 WHEN
+                          ((SELECT     COUNT(*)
+                              FROM         (PARTIDO INNER JOIN
+                                                    DETALLE_PARTIDO ON(DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO)
+                                                    )
+                              WHERE     (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_LOCAL) OR
+                                                    (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_VISITANTE)) >
+                          (SELECT     COUNT(*)
+                            FROM          (PARTIDO INNER JOIN
+                                                   DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO)
+                                                    )
+                            WHERE      (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_VISITANTE) OR
+                                                   (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_LOCAL))) THEN 0 WHEN
+                          ((SELECT     COUNT(*)
+                              FROM         (PARTIDO INNER JOIN
+                                                    DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO)
+                                                    )
+                              WHERE     (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_LOCAL) OR
+                                                    (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_VISITANTE)) =
+                          (SELECT     COUNT(*)
+                            FROM          (PARTIDO INNER JOIN
+                                                   DETALLE_PARTIDO ON (DEPA_PARTIDO = PART_CODIGO) AND
+                                                    (PART_CODIGO = p.PART_CODIGO)
+                                                    )
+                            WHERE      (DEPA_ACCION = 1 AND DEPA_EQUIPO = PART_VISITANTE) OR
+                                                   (DEPA_ACCION = 10 AND DEPA_EQUIPO = PART_LOCAL))) THEN 1 END AS PUNTOS
+FROM PARTIDO P
